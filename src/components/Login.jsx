69036ce7d1ed024f155bad773ffbdad0b67e5a3e
Sara Tesic <sara.tesic5@gmail.com>
2022-08-13 17:34:16 +0200

    Renaming

--- src/components/Login.jsx
+++ src/components/Login.jsx
@@ -2,7 +2,7 @@ import React from 'react'
 import { useState } from "react"
 import axios from 'axios';
 import {useNavigate} from "react-router-dom";
-import '../Login.css';
+import '../LoginAndRegister.css';
 
 const Login = () => {
 
